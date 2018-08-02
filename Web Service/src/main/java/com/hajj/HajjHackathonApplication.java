06e1ba55b342bb814998df35a8026aead1980439
adnredwan77 <adnredwan77@gmail.com>
2018-08-02 18:35:01 +0300

    Web Service

---
+++ Web Service/src/main/java/com/hajj/HajjHackathonApplication.java
@@ -0,0 +1,12 @@
+package com.hajj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajjHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajjHackathonApplication.class, args);
+	}
+}
---
+++ Web Service/src/main/java/com/hajj/facerecognition/javafaces/FaceRecError.java
@@ -0,0 +1,8 @@
+package com.hajj.facerecognition.javafaces;
+
+public class FaceRecError extends Exception {
+
+	public FaceRecError(String msg){
+		super(msg);
+	}
+}
